c6446814fad1e06d3ae75672e79b757b5dd44b23
Gurjas Singh <gurjassingh92@gmail.com>
2021-04-13 17:16:48 +0530

    CNAME updated

--- src/App.js
+++ src/App.js
@@ -13,44 +13,8 @@ import { BrowserRouter as Router, Switch, Route, HashRouter, Link } from "react-
 
 export default function App() {
   
-    return (
-    // <Router>
-      // <Switch>
-      //   <Route path="/blog">
-      //     <BlogIndexPage />
-      //   </Route>
-      //   <Route path="/portfolio">
-      //     <PortfolioPage />
-      //   </Route>
-      //   <Route path="/courses">
-      //     <CoursesPage />
-      //   </Route>
-      //   <Route path="/contact-us">
-      //     <ContactUsPage />
-      //   </Route>
-      //   <Route path="/">
-      //     <HomePage />
-      //   </Route>
-      // </Switch>
-    // </Router>
+  return (
     <HashRouter basename='/'>
-      {/* <div>
-        <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/contact-us">ContactUsPage</Link></li>
-        <li><Link to="/portfolio">PortfolioPage</Link></li>
-        </ul>
-        <hr />
-        <Route exact path="/" component={HomePage} />
-        <Route path="/contact-us" component={ContactUsPage} />
-        <Route path="/portfolio" component={PortfolioPage} />
-      </div> */}
-      {/* <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/contact-us" component={ContactUsPage} />
-        <Route exact path="/portfolio" component={PortfolioPage} />
-        {/* <Route exact path="/portfolio" component={NotFound} /> */}
-      {/* </Switch> */}
       <Route exact path="" component={HomePage} />
       <Route exact path="/contact-us" component={ContactUsPage} />
       <Route exact path="/portfolio" component={PortfolioPage} />
